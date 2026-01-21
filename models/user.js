b5132c3db45f97a55d23483d8d098ebdf2ad420c
Ami <ami.dorsey@me.com>
2026-01-20 19:59:08 -0500

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    hashedPassword: {
+        type: String,
+        required: true
+     }
+    }, {
+    timestamp: true
+    
+});
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    delete returnedObject.hashedPassword;
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
