be4c9334714a90db6ce0ee3c3683c68588d932a9
Ami <ami.dorsey@me.com>
2026-01-25 14:36:06 -0500

    /controllers/users and routes/users animation update

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,11 @@ const verifyToken = require('../middleware/verify-token');
 
 router.get('/', verifyToken, userController.getAllUsers);
 router.get('/:userId', verifyToken, userController.getUserProfile);
+router.get('profile', verifyToken, userController.getMyProfile);
+router.put('/profile', verifyToken, userController.updateMyProfile);
+router.put('/settings', verifyToken, userController.updateSettings);
+router.get('/stats', verifyToken, userController.getMyStats);
+
+router.get('/:userId', verifyToken, userController.getUserProfile);
 
 module.exports = router;
